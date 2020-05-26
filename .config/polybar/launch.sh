83ea4abf14da52bdb3a56f8035f4aa6e088be679
Cerclique <aurelien.vernizeau@gmail.com>
2020-05-26 18:54:47 +0200

    Add polyabr configuration

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bot polybar
+polybar main -c /home/$USER/.config/polybar/config_bot &
+
+# Launch top polybar
+polybar main -c /home/$USER/.config/polybar/config_top &
