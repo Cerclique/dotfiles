ccfdfea92f623656bff4a25abcb4c110b94f78ee
cerclique <aurelien.vernizeau@gmail.com>
2021-05-27 01:41:09 +0200

    Add new font (Mono type) in order to get full icon with polybard. See link below for more information. https://github.com/polybar/polybar/issues/991

--- .config/polybar/scripts/spotify.py
+++ .config/polybar/scripts/spotify.py
@@ -68,7 +68,7 @@ if args.play_pause is not None:
 try:
     session_bus = dbus.SessionBus()
     spotify_bus = session_bus.get_object(
-        'org.mpris.MediaPlayer2.spotify',
+        'org.mpris.MediaPlayer2.spotifyd',
         '/org/mpris/MediaPlayer2'
     )
 
@@ -120,4 +120,4 @@ except Exception as e:
     if isinstance(e, dbus.exceptions.DBusException):
         print('')
     else:
-        print(e)
+        print('')
